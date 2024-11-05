e5dbdad82a3820e7b986cbc632268cb27c8b5f93
Emanuel de Jong <44051853+Emanuel-de-Jong@users.noreply.github.com>
2024-11-06 00:06:12 +0100

    Fix bat

--- Playlist-Mix-Player/!!rnd_mix.bat
+++ Playlist-Mix-Player/!!rnd_mix.bat
@@ -1,4 +1,5 @@
-GOTO :PROGRAM_START
+@echo off
+GOTO PROGRAM_START
 Chill Old Rock
 Chill Rock
 
@@ -6,4 +7,4 @@ https://www.youtube.com/watch?v=abc1234xyz
 https://open.spotify.com/playlist/1a2b3c4d5e6f7g8h9i0j?si=1234567890abcdef
 :PROGRAM_START
 
-"%PLAYLIST_MIX_PLAYER_PATH%\\Playlist-Mix-Player.exe"
+"%PLAYLIST_MIX_PLAYER_PATH%\Playlist-Mix-Player.exe"
