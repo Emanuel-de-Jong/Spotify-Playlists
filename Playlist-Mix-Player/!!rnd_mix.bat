9da6ff62a89d1bb6708ca32384614fb544f49d5f
Emanuel de Jong <44051853+Emanuel-de-Jong@users.noreply.github.com>
2024-11-06 01:16:43 +0100

    .

--- Playlist-Mix-Player/!!rnd_mix.bat
+++ Playlist-Mix-Player/!!rnd_mix.bat
@@ -6,4 +6,4 @@ https://www.youtube.com/watch?v=3qLFjfGWmNg
 https://www.youtube.com/watch?v=9TbEbzhwmMY
 :PROGRAM_START
 
-"%PLAYLIST_MIX_PLAYER_PATH%\Playlist-Mix-Player.exe"
+"%PLAYLIST_MIX_PLAYER_PATH%\Playlist-Mix-Player.exe" "Acoustic"
