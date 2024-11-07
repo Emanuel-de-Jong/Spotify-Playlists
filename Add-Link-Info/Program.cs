3a7dcf1a5b327364ff66b495c0bb6fcc536e950b
Emanuel de Jong <44051853+Emanuel-de-Jong@users.noreply.github.com>
2024-11-07 13:16:12 +0100

    Add_Link_Info

---
+++ Add-Link-Info/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Add_Link_Info
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().Run(args);
+        }
+
+        private void Run(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
