4aa5adb9893b04493b5ffae8ca769faeee596c0f
Emanuel de Jong <44051853+Emanuel-de-Jong@users.noreply.github.com>
2024-11-20 19:20:34 +0100

    Spotify login improvements

--- Playlist-Merger/Program.cs
+++ Playlist-Merger/Program.cs
@@ -6,7 +6,8 @@ namespace Playlist_Merger
 {
     public class Program
     {
-        private IDeserializer yamlReader;
+        private ISerializer serializer;
+        private IDeserializer deserializer;
         private MergePlaylistsDeps mergePlaylistDeps;
         private Playlists oldMixPlaylists = [];
         private Playlists oldMergePlaylists = [];
@@ -15,14 +16,15 @@ namespace Playlist_Merger
         private Playlists mixPlaylists;
         private Playlists mergePlaylists;
 
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             new Program().Run().GetAwaiter().GetResult();
         }
 
         private async Task Run()
         {
-            yamlReader = new DeserializerBuilder().Build();
+            serializer = new SerializerBuilder().Build();
+            deserializer = new DeserializerBuilder().Build();
 
             LoadMergePlaylistsDeps();
             LoadOldPlaylists();
@@ -34,7 +36,7 @@ namespace Playlist_Merger
         private void LoadMergePlaylistsDeps()
         {
             string yamlContent = File.ReadAllText("Merge-Playlists-Deps.yaml");
-            mergePlaylistDeps = yamlReader.Deserialize<MergePlaylistsDeps>(yamlContent);
+            mergePlaylistDeps = deserializer.Deserialize<MergePlaylistsDeps>(yamlContent);
         }
 
         private void LoadOldPlaylists()
@@ -42,30 +44,83 @@ namespace Playlist_Merger
             if (File.Exists("Mix-Playlists.yaml"))
             {
                 string yamlContent = File.ReadAllText("Mix-Playlists.yaml");
-                oldMixPlaylists = yamlReader.Deserialize<Playlists>(yamlContent);
+                oldMixPlaylists = deserializer.Deserialize<Playlists>(yamlContent);
             }
 
             if (File.Exists("Merge-Playlists.yaml"))
             {
                 string yamlContent = File.ReadAllText("Merge-Playlists.yaml");
-                oldMergePlaylists = yamlReader.Deserialize<Playlists>(yamlContent);
+                oldMergePlaylists = deserializer.Deserialize<Playlists>(yamlContent);
             }
         }
 
-        private void CreateSpotifyClient()
+        private async Task CreateSpotifyClient()
         {
             string yamlContent = File.ReadAllText("Spotify-API.yaml");
-            Dictionary<string, string> apiCredentials = yamlReader.Deserialize<Dictionary<string, string>>(yamlContent);
-            SpotifyClientConfig spotifyClientConfig = SpotifyClientConfig
-                .CreateDefault()
-                .WithAuthenticator(new ClientCredentialsAuthenticator(
-                    apiCredentials["SpotifyClientId"],
-                    apiCredentials["SpotifyClientSecret"]));
-            spotifyClient = new SpotifyClient(spotifyClientConfig);
+            Dictionary<string, string> apiCredentials = deserializer.Deserialize<Dictionary<string, string>>(yamlContent);
+            string clientId = apiCredentials["ClientId"];
+            string clientSecret = apiCredentials["ClientSecret"];
+            string? refreshToken = apiCredentials.ContainsKey("RefreshToken") ? apiCredentials["RefreshToken"] : null;
+            string? accessToken = null;
+
+            if (refreshToken != null)
+            {
+                AuthorizationCodeRefreshResponse tokenResponse = await new OAuthClient().RequestToken(
+                    new AuthorizationCodeRefreshRequest(
+                        clientId,
+                        clientSecret,
+                        refreshToken
+                    )
+                );
+
+                if (!tokenResponse.IsExpired)
+                {
+                    accessToken = tokenResponse.AccessToken;
+                    apiCredentials["RefreshToken"] = tokenResponse.RefreshToken;
+                }
+            }
+
+            if (accessToken == null)
+            {
+                LoginRequest loginRequest = new(
+                    new Uri("http://localhost:5000/callback"),
+                    clientId,
+                    LoginRequest.ResponseType.Code)
+                {
+                    Scope = { Scopes.PlaylistReadPrivate }
+                };
+
+                Uri uri = loginRequest.ToUri();
+                Console.WriteLine("Please log in via: " + uri);
+
+                string code = "TODO";
+
+                AuthorizationCodeTokenResponse tokenResponse = await new OAuthClient().RequestToken(
+                    new AuthorizationCodeTokenRequest(
+                        clientId,
+                        clientSecret,
+                        code,
+                        new Uri("http://localhost:5000/callback")
+                    )
+                );
+
+                accessToken = tokenResponse.AccessToken;
+                apiCredentials["RefreshToken"] = tokenResponse.RefreshToken;
+            }
+
+            spotifyClient = new SpotifyClient(accessToken);
+
+            yamlContent = serializer.Serialize(apiCredentials);
+            File.WriteAllText("Spotify-API.yaml", yamlContent);
         }
 
         private async Task FetchPlaylistMetas()
         {
+            Paging<FullPlaylist> playlists = await spotifyClient.Playlists.CurrentUsers();
+            foreach (FullPlaylist playlist in playlists.Items)
+            {
+                Console.WriteLine($"Playlist Name: {playlist.Name}, Is Public: {playlist.Public}");
+            }
         }
     }
 }
